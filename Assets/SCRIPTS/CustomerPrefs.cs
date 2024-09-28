5e01a7cc3114b56fbe5cc72e6322b749a26e1911
JacksonSommer <jpsinfl@gmail.com>
2024-09-28 17:40:23 -0400

    Speech Bubble and Customer Prefs
    -temporarily disabled the conveyer named "counter" because it is unnecessary for the prototype (potions created by the cauldron exist outside of the parent object of either screen, so switching screens automatically brings potions over)
    -added a speech bubble to display a customer's order, with open slots for 2 ingredients and a final product to be visualized
    -Added a collider to the default customer and modified DragBottle2D.cs so that when a player drags a bottle to the customer, the bottle throws a debug line
    -created pseudocode for CustomerPrefs.cs which will be assigned to each customer as a prefab, this will determine what potion the customer wants and if they are receiving the correct potion

---
+++ Assets/SCRIPTS/CustomerPrefs.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerPrefs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Display 1st ingredient in speech bubble
+
+        //Display 2nd ingredient in speech bubble
+
+        //Display finished product in speech bubble
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if correct potion tag is dragged over player
+            //display success UI
+        //else
+            //display fail UI
+    }
+}
